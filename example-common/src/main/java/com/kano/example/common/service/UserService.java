5c94f7cd7ea33fac28d82af24506ffae4d2d26fd
kano <ch16601974547@163.com>
2025-10-04 09:23:13 +0800

    提交all

---
+++ example-common/src/main/java/com/kano/example/common/service/UserService.java
@@ -0,0 +1,17 @@
+package com.kano.example.common.service;
+
+import com.kano.example.common.model.User;
+
+/**
+ * 用户服务
+ */
+public interface UserService {
+
+    /**
+     * 获取用户
+     *
+     * @param user
+     * @return
+     */
+    User getUser(User user);
+}
---
+++ example-provider/src/main/java/com/kano/example/provider/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.kano.example.provider;
+
+
+import com.kano.example.common.model.User;
+import com.kano.example.common.service.UserService;
+
+/**
+ * 用户服务实现类
+ */
+public class UserServiceImpl implements UserService {
+
+    public User getUser(User user) {
+        System.out.println("用户名：" + user.getName());
+        return user;
+    }
+}
